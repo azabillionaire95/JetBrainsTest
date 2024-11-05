7d5d0d8d57877e3f0477f9cd30a987b21c673390
azabillionaire95 <azabillionaire@gmail.com>
2024-11-05 17:05:43 +0500

    добавил на стр теста аннотацию ExtendWith

--- src/test/java/com/example/jetbrainstest/tests/RiderPageTest.java
+++ src/test/java/com/example/jetbrainstest/tests/RiderPageTest.java
@@ -1,11 +1,14 @@
 package com.example.jetbrainstest.tests;
 
+import com.example.jetbrainstest.MyExtension;
 import com.example.jetbrainstest.pages.RiderPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(MyExtension.class)
 public class RiderPageTest extends BaseTest {
     private RiderPage rp;
 
