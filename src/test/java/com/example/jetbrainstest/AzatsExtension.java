74ee944898925a187d3fe453cd0396d705cb1e9a
azabillionaire95 <azabillionaire@gmail.com>
2024-11-10 16:45:52 +0500

    удалил ненужный код в AzatsExtension

--- src/test/java/com/example/jetbrainstest/AzatsExtension.java
+++ src/test/java/com/example/jetbrainstest/AzatsExtension.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class AzatsExtension implements AfterTestExecutionCallback {
     @Override
-    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
+    public void afterTestExecution(ExtensionContext extensionContext) {
         if (extensionContext.getExecutionException().isPresent()) AllureAttachmentsManager.screenshot();
     }
 }
